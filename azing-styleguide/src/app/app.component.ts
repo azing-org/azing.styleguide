bbf833b662c12ddd5a380859324dd8f3347a5f8d
Christoph Bühler <christoph.buehler@hotmail.com>
2018-07-13 17:34:08 +0200

    added theme 2

--- azing-styleguide/src/app/app.component.ts
+++ azing-styleguide/src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
     ['600', '#546271'],
     ['700', '#444f5b'],
     ['800', '#353d46'],
-    ['900', '#23292f']
+    ['900', '#23292f'],
   ];
   accentColors = [
     ['50', '#ffebef'],
@@ -28,8 +28,20 @@ export class AppComponent {
     ['600', '#f02e38'],
     ['700', '#dd2232'],
     ['800', '#d0192a'],
-    ['900', '#c2001d']
+    ['900', '#c2001d'],
   ];
+  primary2Colors = [
+    ['50', '#ede7f9'],
+    ['100', '#d0c3f1'],
+    ['200', '#b09be9'],
+    ['300', '#8f72e1'],
+    ['400', '#7453da'],
+    ['500', '#5634d3'],
+    ['600', '#4b2fcd'],
+    ['700', '#3828c4'],
+    ['800', '#2122bd'],
+    ['900', '#0015b3'],
+  ]
   fontsLink = `<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700|Open+Sans:400,600,700|Material+Icons" rel="stylesheet">`;
   themeScssLink = `https://github.com/azing-org/azing.styleguide/blob/master/azing-styleguide/src/theme.scss`;
   themeCssLink = `https://azing-org.github.io/azing.styleguide/azing-styleguide/theme.css`;
